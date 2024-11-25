841dc4654b4f82b50265526b6ed897db771fe181
KelvinErlangga <kelvinerlanggaa@gmail.com>
2024-11-25 23:46:27 +0700

    web.php update

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 })->name('home');
 
 Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
